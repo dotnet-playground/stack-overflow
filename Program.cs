bd1d39076b65edfb1cbd1eeb7b3b3f9bc9b7c843
Omelian Levkovych <omelianai@gmail.com>
2022-05-28 00:26:49 +0300

    stackoverflow via stack is actually oveflow or called throw the code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void MakeInfiniteRecursion(int x)
+{
+    Console.WriteLine(x);
+    MakeInfiniteRecursion(++x);
+}
+
+
+static void ThrowStackOverflowViaCLR()
+{
+    try
+    {
+        MakeInfiniteRecursion(1);
+    }
+    catch(StackOverflowException exe)
+    {
+        // Won't work. Only if the host env will specifically allow stack overflow exception to be handled.
+        Console.WriteLine(exe);
+    }
+}
+
+static void ThrowStackOveflowViaCode()
+{
+    try
+    {
+        throw new StackOverflowException("Its me, stack oveflow exception!");
+    }
+    catch(StackOverflowException exe)
+    {
+        // This works.
+        Console.WriteLine(exe);
+    }
+}
+
+ThrowStackOveflowViaCode();
+
